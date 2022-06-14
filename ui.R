2c325f1e20a86f18f051a572c2e32eb4020392db
Berkantyuks <byuksektepe@hotmail.com>
2022-06-15 02:20:10 +0300

    Added badges

--- ui.R
+++ ui.R
@@ -44,11 +44,29 @@ fluidPage(
             ),
           ),
 
-          column(12,
+          column(12, class = "text-center",
             tags$br(),
             hr(),
             tags$p("This page and its data are completely open source, developed with R and Love ❤.", style="text-align: center;"),
-            p("© Copyright FaustX Technology all rights reserved." ,style="text-align: center;")
+            p("© Copyright 2022 FaustX Technology all rights reserved. CC BY-NC 4.0 International License." ,style="text-align: center;"),
+            hr(),
+            
+            
+          ),
+          column(12, class = "text-center",
+                 
+                 fillRow( flex = 1, align = "center", width="50vw", style="margin: 0 auto;",
+                          
+                          a(href="https://www.faustx.com/en/fx1-tests", target="_blank", style="display: table;", rel="ai",img(style="border-width: 0;margin-bottom:25px;",width="88px", src="https://www.faustx.com/img/fx-stat/114x40-one-ai.png", alt="FX-1-AI")),
+                          a(href="https://www.faustx.com/en/fx1-tests", target="_blank", style="display: table;", rel="fcp",img(style="border-width: 0;margin-bottom:25px;",width="88px", src="https://www.faustx.com/img/fx-stat/114x40-fcp.png", alt="FX-1-AI")),
+                          a(href="https://github.com/Berkantyuks/FaustX-Flight-Records", target="_blank", style="display: table;", rel="git",img(style="border-width: 0;margin-bottom:25px;",width="88px", src="https://www.faustx.com/img/fx-stat/114x40-git.png", alt="FX-1-GIT")),
+                          a(href="http://creativecommons.org/licenses/by-nc/4.0/", target="_blank", style="display: table;", rel="license",img(style="border-width: 0;margin-bottom:25px;", src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png", alt="Creative Commons License")),
+                          img(style="border-width: 0;margin-bottom:25px;", src="https://www.faustx.com/img/fx-stat/114x40-r-bg-b.png",width="88px", alt="FX-R-BG"),
+                          img(style="border-width: 0;margin-bottom:25px;", src="https://www.faustx.com/img/fx-stat/114x40-shiny.png",width="88px", alt="FX-SH-BG")
+                          
+                 )
+                 
           )
+
           
 )
